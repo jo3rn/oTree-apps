e1464ae73a01ca56b4a537b5bed89fd5fd67cb2b
jo3rn <jo3rn@users.noreply.github.com>
2019-02-02 13:06:04 +0100

    Add app to session config

--- settings.py
+++ settings.py
@@ -12,12 +12,12 @@ SESSION_CONFIG_DEFAULTS = {
 }
 
 SESSION_CONFIGS = [
-    #{
-    #    'name': 'public_goods',
-    #    'display_name': "Public Goods",
-    #    'num_demo_participants': 3,
-    #    'app_sequence': ['public_goods', 'payment_info'],
-    #},
+    {
+        'name': 'Devils_Task',
+        'display_name': "Devils Task",
+        'num_demo_participants': 1,
+        'app_sequence': ['OneByOne'],
+    },
 ]
 
 
