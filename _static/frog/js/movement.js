dfb3ff2ff0a5331868002eb917def66b9d4c340e
jo3rn <jo3rn@users.noreply.github.com>
2019-04-16 20:57:59 +0200

    Let frog jump

--- _static/frog/js/movement.js
+++ _static/frog/js/movement.js
@@ -1,21 +1,50 @@
+const container = document.getElementById('container');
 const frog = document.getElementById('frog');
+
+
+const containerRect = container.getBoundingClientRect();
+// container is quadratic: length = height = width
+// containerLength declares the maximum distance a frog can jump
+const containerLength = containerRect.right - containerRect.left;
+console.log("container length: " + containerLength);
+
 
 frog.addEventListener('click', getClickPosition, false);
 
 function getClickPosition(e) {
+    frog.removeEventListener('click', getClickPosition);
     const x = e.clientX;
     const y = e.clientY;
     console.log(x, y);
-    const rect = frog.getBoundingClientRect();
-    const left = rect.left;
-    const right = rect.right;
-    const top = rect.top;
-    const bottom = rect.bottom;
-    console.log(`left: ${left}\ntop: ${top}\nright: ${right}\nbottom: ${bottom}`);
-    const middleX = (right + left) / 2;
-    const middleY = (bottom + top) /2;
-    console.log(`middle: ${middleX}, ${middleY}`);
+
+    const frogRect = frog.getBoundingClientRect();
+    const frogLeft = frogRect.left;
+    const frogRight = frogRect.right;
+    const frogTop = frogRect.top;
+    const frogBottom = frogRect.bottom;
+    console.log(`Frog:\nleft: ${frogLeft}\nfrogTop: ${frogTop}\nright: ${frogRight}\nbottom: ${frogBottom}\n`);
+    const frogWidth = frogRight - frogLeft;
+    const frogHeight = frogBottom - frogTop;
+    const middleX = (frogRight + frogLeft) / 2;
+    const middleY = (frogBottom + frogTop) /2;
+    console.log(`Frog center: ${middleX}, ${middleY}`);
+
     const leftOffsetToMiddle = middleX - x;
     const topOffsetToMiddle = middleY - y;
+
+    const percentageToLeft = leftOffsetToMiddle / (frogWidth / 2);
+    const percentageToTop = topOffsetToMiddle / (frogHeight / 2);
     console.log(`leftOffsetToMiddle: ${leftOffsetToMiddle}\ntopOffsetToMiddle: ${topOffsetToMiddle}`);
+    console.log(`percentageToLeft: ${percentageToLeft}\npercentageToTop: ${percentageToTop}`);
+
+    const movementToLeft = Math.floor(percentageToLeft * containerLength);
+    const movementToTop = Math.floor(percentageToTop * containerLength);
+
+    const transform = `translate(${movementToLeft}px, ${movementToTop}px)`;
+    moveFrog(transform);
 }
+
+const moveFrog = function (transformation){
+    console.log(transformation);
+    frog.style.transform = transformation;
+};
