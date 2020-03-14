75695001a3f4bad020d9f8709b4e6c460ace60a1
jo3rn <jo3rn@users.noreply.github.com>
2020-03-14 16:08:33 +0100

    Add second try if frog didnt hit the pond
    no 2nd try if frog jumped (partly) out of screen

--- _static/frog/js/movement.js
+++ _static/frog/js/movement.js
@@ -4,34 +4,37 @@ const frog = document.getElementById('frog');
 const frogInPond = document.getElementById('frog_success');
 
 const containerRect = container.getBoundingClientRect();
-// container is quadratic: length = height = width
+// container is quadratic: length == height == width
 // containerLength declares the maximum distance a frog can jump
 const containerLength = containerRect.right - containerRect.left;
+
+// coordinates of pond box
+const pondRect = pond.getBoundingClientRect();
+const pondLeft = pondRect.left;
+const pondRight = pondRect.right;
+const pondTop = pondRect.top;
+const pondBottom = pondRect.bottom;
 
 let splashAudio = new Audio();
 let quakAudio = new Audio();
 let reminderAudio = new Audio();
 
 let noInteraction = true;
+let isFirstTry = true;
 
-frog.addEventListener('click', getClickPosition, false);
+// difficulty: how far can the center of the frog be outside of the pond box (absolute value)
+// 0 means frog center needs to be within the pond box
+// a lower value x means the frog center can be x px outside of the pond box (e.g. -20 -> 20px)
+const difficulty = - 25;
 
-function initializeJs(pathToSplashAudio, pathToQuakAudio, pathToReminderAudio) {
-    splashAudio = new Audio(pathToSplashAudio);
-    quakAudio = new Audio(pathToQuakAudio);
-    reminderAudio = new Audio(pathToReminderAudio);
-    randomizeFrogPosition();
-    setTimeout(playReminder, 20000);
-}
-
-function randomizeFrogPosition() {
+const randomizeFrogPosition = () => {
     const minX = 0;
-    // frog should be a decent distance away from the pond
+    // frog should be a decent distance (60%) away from the pond
     // => shift top boundary to bottom
     const minY = Math.ceil(containerLength - 0.6 * containerLength);
-    // frog is 15% the size of the container
-    // => shift right boundary 15% to the left
-    const max = Math.floor(containerLength - 0.15 * containerLength);
+    // frog width needs to be taken into account when positioning by left coordinate
+    // => shift right boundary to the left
+    const max = Math.floor(containerLength - frog.offsetWidth);
     
     // compute random X and Y coordinates for frog
     const randomX = Math.floor(Math.random() * (max - minX + 1) + minX);
@@ -43,13 +46,21 @@ function randomizeFrogPosition() {
     frog.style.visibility = "visible";
 }
 
-function getClickPosition(e) {
+const initializeJs = (pathToSplashAudio, pathToQuakAudio, pathToReminderAudio) => {
+    splashAudio = new Audio(pathToSplashAudio);
+    quakAudio = new Audio(pathToQuakAudio);
+    reminderAudio = new Audio(pathToReminderAudio);
+    randomizeFrogPosition();
+    setTimeout(playReminder, 20000);
+}
+
+const getClickPosition = (e) => {
     frog.removeEventListener('click', getClickPosition);
     noInteraction = false;
 
     // coordinates of click position on frog
-    const x = e.clientX;
-    const y = e.clientY;
+    const clickX = e.clientX;
+    const clickY = e.clientY;
 
     // coordinates of frog box
     const frogRect = frog.getBoundingClientRect();
@@ -60,41 +71,46 @@ function getClickPosition(e) {
 
     // coordinates of frog center
     const middleX = (frogRight + frogLeft) / 2;
-    const middleY = (frogBottom + frogTop) /2;
+    const middleY = (frogBottom + frogTop) / 2;
 
     // how far away is the click from the middle of the frog (as absolute value)?
-    const leftOffsetToMiddle = middleX - x;
-    const topOffsetToMiddle = middleY - y;
+    const leftOffsetToMiddle = middleX - clickX;
+    const topOffsetToMiddle = middleY - clickY;
     // width and height of the frog
     const frogWidth = frogRight - frogLeft;
     const frogHeight = frogBottom - frogTop;
+
     // how far away is the click from the middle of the frog (relative to the frog size)?
     const percentageToLeft = leftOffsetToMiddle / (frogWidth / 2);
     const percentageToTop = topOffsetToMiddle / (frogHeight / 2);
 
     // how far should the frog jump:
     // frog jumps farther if the click is farther away from the middle of the frog
-    const movementToLeft = Math.floor(percentageToLeft * containerLength);
-    const movementToTop = Math.floor(percentageToTop * containerLength);
+    const movementToRight = Math.floor(percentageToLeft * containerLength);
+    const movementToBottom = Math.floor(percentageToTop * containerLength);
+
+    console.log(`movementToRight: ${movementToRight}`)
+    console.log(`movementToBottom: ${movementToBottom}`)
 
     // lets the frog jump
-    const transform = `translate(${movementToLeft}px, ${movementToTop}px)`;
-    moveFrog(transform);
+    moveFrogNew(movementToRight, movementToBottom)
 }
 
-const moveFrog = function (transformation){
-    frog.style.transform = transformation;
-    setTimeout(checkIfFrogIsInPond, 1400);
-};
+const moveFrogNew = (right, bottom) => {
+    frog.style.left = (frog.offsetLeft + right) + "px";
+    frog.style.top = (frog.offsetTop + bottom) + "px";
+    setTimeout(proceedAfterMovement, 1400);
+}
 
-function checkIfFrogIsInPond() {
-    // coordinates of pond box
-    const pondRect = pond.getBoundingClientRect();
-    const pondLeft = pondRect.left;
-    const pondRight = pondRect.right;
-    const pondTop = pondRect.top;
-    const pondBottom = pondRect.bottom;
+const proceedAfterMovement = () => {
+    if (isFrogInPond()) {
+        frogSucceeded();
+    } else {
+        frogMissed();
+    }
+}
 
+const isFrogInPond = () => {
     // coordinates of settled frog box
     const frogRect = frog.getBoundingClientRect();
     const frogLeft = frogRect.left;
@@ -102,33 +118,66 @@ function checkIfFrogIsInPond() {
     const frogTop = frogRect.top;
     const frogBottom = frogRect.bottom;
 
+    console.log(`frogRight: ${frogRight}`)
+    console.log(`frogBottom: ${frogBottom}`)
+
     // coordinates of settled frog center
     const frogCenterX = (frogRight + frogLeft) / 2;
-    const frogCenterY = (frogBottom + frogTop) /2;
-
-    // difficulty: how far can the center of the frog be outside of the pond box (absolute value)
-    // 0 means frog center needs to be within the pond box
-    // a lower value (e.g. -20) means the frog center can be 20px outside of the pond box
-    const difficulty = - 20;
+    const frogCenterY = (frogBottom + frogTop) / 2;
 
     // check whether frog center is within acceptable box
     if (frogCenterX <= pondLeft + difficulty ||
         frogCenterY >= pondBottom - difficulty ||
         frogCenterX >= pondRight - difficulty ||
         frogCenterY <= pondTop + difficulty) {
-        quakAudio.play();
-        frogInPond.value = 0;
-        frog.style.backgroundColor = "#94CC00";
-        frog.style.transform = "rotate(360deg)";
+        return false;
     } else {
-        splashAudio.play();
-        frogInPond.value = 1;
+        return true;
     }
-    frog.style.opacity = "0";
+}
+
+const frogMissed = () => {
+    if (!isFirstTry || hasFrogJumpedOutOfWindow()) {
+        frogInPond.value = 0;
+        animateFrogFail();
+        setTimeout(advanceToNextPage, 1500);
+    } else {
+        frog.addEventListener('click', getClickPosition);
+        animateFrogSecondTry();
+        isFirstTry = false;
+    }
+}
+
+const hasFrogJumpedOutOfWindow = () => {
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+    const frogRect = frog.getBoundingClientRect();
+    return frogRect.top < -1 || frogRect.left < -1 || frogRect.right > windowWidth || frogRect.bottom > windowHeight;
+}
+
+const frogSucceeded = () => {
+    frogInPond.value = 1;
+    animateFrogSuccess();
     setTimeout(advanceToNextPage, 1500);
 }
 
-function playReminder() {
+const animateFrogSecondTry = () => {
+    frog.style.borderColor = 'blue';
+}
+
+const animateFrogFail = () => {
+    quakAudio.play();
+    frog.style.backgroundColor = "#94CC00";
+    frog.style.transform = "rotate(360deg)";
+    frog.style.opacity = "0";
+}
+
+const animateFrogSuccess = () => {
+    splashAudio.play();
+    frog.style.opacity = "0";
+}
+
+const playReminder = () => {
     if (noInteraction) {
         reminderAudio.play().then(() => {
             setTimeout(playReminder, 20000);
@@ -139,6 +188,8 @@ function playReminder() {
     }
 }
 
-function advanceToNextPage() {
+const advanceToNextPage = () => {
   document.getElementById("form").submit();
 }
+
+frog.addEventListener('click', getClickPosition, false);
